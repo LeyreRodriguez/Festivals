7d3671e362b1e8fabc50fbbae97bc60ba8182eba
MinervaQuin <minervaqf8@gmail.com>
2022-03-25 20:16:43 +0000

    json-server CRUD

---
+++ archivos html y css/json/js/get_request.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+axios.get('http://localhost:3000/Festivales')
+    .then(resp => {
+        data = resp.data;
+        data.forEach(e => {
+            console.log(`${e.id}, ${e.nombre}, ${e.informacion},${e.fecha}, ${e.foto}, ${e.precio},${e.lugar}`);
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
