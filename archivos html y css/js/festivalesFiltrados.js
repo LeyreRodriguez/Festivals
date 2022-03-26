a54b9e2bec5a3218c57a3a58ee5510930cd3856e
LeyreRodriguez <leyre.rodriguez104@alu.ulpgc.es>
2022-03-26 13:08:35 +0000

    Correcciones realizadas

--- archivos html y css/js/festivalesFiltrados.js
+++ archivos html y css/js/festivalesFiltrados.js
@@ -1,4 +1,4 @@
-const loadProduct = async function() {
+const loadFestival = async function() {
     await $.getJSON("json/festivales.json", function(json) {
         Object.entries(json.Festivales).forEach((entry) => {
             const [key, f] = entry;
@@ -6,32 +6,28 @@ const loadProduct = async function() {
 
             const div = document.createElement('div');
             const div1 = document.createElement('div');
+            const a = document.createElement('a');
             const img = document.createElement('img');
             const p = document.createElement('p');
-            const p1 = document.createElement('p');
-            const p2 = document.createElement('p');
-            const p3 = document.createElement('p');
+
 
             div.setAttribute('class', 'festival123');
             div1.setAttribute('class', 'datos');
+            a.setAttribute('href',"Festival.html" );
             img.setAttribute('src', f.foto);
-            p.textContent = f.nombre;
-            p1.textContent = f.lugar;
-            p2.textContent = f.fecha;
-            p3.textContent = f.precio;
+            p.textContent = `${f.nombre}, ${f.lugar}, ${f.fecha}, ${f.precio}` ;
+
 
             div.appendChild(div1);
-            div1.appendChild(img);
+            div1.appendChild(a);
+            a.appendChild(img);
             div1.appendChild(p);
-            div1.appendChild(p1);
-            div1.appendChild(p2);
-            div1.appendChild(p3);
             var sec = document.getElementById('filtro');
             sec.appendChild(div);
         })
     })
 }
-loadProduct();
+loadFestival();
 
 
 
