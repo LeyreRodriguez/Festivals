1d0a6927ffd025d8ca7b1350f84cf6d34a4ea9b3
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-05-03 16:16:40 +0100

    Add files via upload

--- festival/src/app/services/inscripcion-puesto-venta.service.ts
+++ festival/src/app/services/inscripcion-puesto-venta.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InscripcionPuestoVentaService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  guardarVenta(puestoVenta: any): Promise<any> {
+    return this.firestore.collection('puestoVenta').add(puestoVenta);
+  }
 }
