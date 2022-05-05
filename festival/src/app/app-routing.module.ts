4bf5d09c2699f01cb8cb35eb168f0747c46fc8eb
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-05-05 20:10:24 +0100

    Update app-routing.module.ts

--- festival/src/app/app-routing.module.ts
+++ festival/src/app/app-routing.module.ts
@@ -5,23 +5,27 @@ import { FestivalComponent } from './components/festival/festival.component';
 import { ErrorComponent } from './components/error/error.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { FormularioCompraEntradasComponent } from './components/formulario-compra-entradas/formulario-compra-entradas.component';
-import { FormularioEnviadoComponent } from './components/formulario-enviado/formulario-enviado.component';
-import { PagoRealizadoComponent } from './components/pago-realizado/pago-realizado.component';
 import {FormularioConcursoComponent} from './components/formulario-concurso/formulario-concurso.component';
-
+import { InscripcionPuestoVentaComponent } from './components/inscripcion-puesto-venta/inscripcion-puesto-venta.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SignupUsersComponent } from './components/signup-users/signup-users.component';
+import { SignupBusinessComponent } from './components/signup-business/signup-business.component';
 
 const rutas: Routes = [
-  // Ver si esto hace que sea el startPoint
-  { path: '', redirectTo: 'formularioConcursso', pathMatch: 'full' },
-  { path: 'festivales', component: FestivalesComponent },
-  { path: 'mainPage', component: MainPageComponent },
-  { path: 'festival', component: FestivalComponent },
-  { path: 'festival/:id', component: FestivalComponent },
-  { path: 'formularioCompraEntradas', component: FormularioCompraEntradasComponent },
-  { path: 'formularioEnviado', component: FormularioEnviadoComponent },
-  { path: 'formularioConcursso', component: FormularioConcursoComponent },
-  { path: 'pagoRealizado', component: PagoRealizadoComponent },
-  { path: 'formularioConcursso', component: ErrorComponent }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'festivales', component: FestivalesComponent },
+    { path: 'mainPage', component: MainPageComponent },
+    { path: 'festival', component: FestivalComponent },
+    { path: 'festival/:id', component: FestivalComponent },
+    { path: 'concurso', component: FormularioConcursoComponent },
+    { path: 'entradas', component: FormularioCompraEntradasComponent },
+    { path: 'venta', component: InscripcionPuestoVentaComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'signupUsers', component: SignupUsersComponent },
+    { path: 'signupBussines', component: SignupBusinessComponent },
+    { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
