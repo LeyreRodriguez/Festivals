195e8370ba1ea3b2608d001a1ef824017b8df32b
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-05-08 16:04:47 +0100

    Add files via upload

---
+++ festival/src/app/components/formulario-concurso/formulario-concurso.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { concurso } from 'src/app/models/concurso/concurso.module';
+import { ConcursoService } from './../../services/concurso.service';
+
+@Component({
+  selector: 'app-formulario-concurso',
+  templateUrl: './formulario-concurso.component.html',
+  styleUrls: ['./formulario-concurso.component.css']
+})
+export class FormularioConcursoComponent implements OnInit {
+  concursoForm: FormGroup;
+  loading = false;
+  overlay = false;
+  id: string | undefined;
+  titulo = 'Agregar Tarjeta';
+  listConcuros: concurso[] = [];
+
+  constructor(
+        private fb: FormBuilder,
+        private concursoService: ConcursoService,
+        private toastr: ToastrService,
+        ) {
+    this.concursoForm = this.fb.group({
+      inputContestName: ['', Validators.required],
+      inputName: ['', Validators.required],
+      inputLastName: ['', Validators.required],
+      inputDNI: ['', Validators.required, Validators.pattern(/([0-9]{7,8}[A-Z])/)],
+      inputContactNumber: ['', [Validators.required, Validators.pattern(/(\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}/)]],
+      inputEmail: ['', [Validators.required, Validators.pattern(/[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}/)]]
+    })
+   }
+
+  ngOnInit(): void {
+    this.obtenerConcursos();
+  }
+
+  agregarConcurso() {
+
+    const concurso: any = {
+      inputContestName: this.concursoForm.value.inputContestName,
+      inputName: this.concursoForm.value.inputName,
+      inputLastName: this.concursoForm.value.inputLastName,
+      inputDNI: this.concursoForm.value.inputDNI,
+      inputContactNumber: this.concursoForm.value.inputContactNumber,
+      inputEmail: this.concursoForm.value.inputEmail,
+    }
+
+    this.loading = true;
+    this.concursoService.guardarConcurso(concurso).then(() => {
+      this.loading = false;
+      this.overlay = true;
+      console.log('tarjeta registrado');
+      this.toastr.success('El usuario fue registrado con exito en el concurso!', 'Concurso registrado')
+      this.concursoForm.reset();
+    }, error => {
+      this.loading = false;
+      this.toastr.error('Opps.. ocurrio un error', 'Error');
+      console.log(error);
+    })
+  }
+
+  guardarConcurso() {
+    if(this.id === undefined) {
+      this.agregarConcurso();
+    }
+  }
+
+  obtenerConcursos(){
+    this.concursoService.obtenerConcursos().subscribe(doc => {
+      this.listConcuros = [];
+      doc.forEach((element: any) => {
+        this.listConcuros.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        })
+      });
+      console.log(this.listConcuros);
+    })
+  }
+}
