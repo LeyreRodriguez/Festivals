abb575ec167d617ba5208fd5c4ce46ceab6144bc
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-05-08 21:25:31 +0100

    Add files via upload

---
+++ festival/src/app/models/buscador/buscador.module.ts
@@ -0,0 +1,9 @@
+export class  buscador {
+  nameType: string;
+
+  constructor(
+    nameType: string,
+    ){
+      this.nameType = nameType;
+  }
+}
---
+++ festival/src/app/models/comentario/comentario.module.ts
@@ -0,0 +1,12 @@
+export class  comentario {
+  userName: string;
+  comentario: string;
+
+  constructor(
+    userName: string,
+    comentario: string,
+    ){
+      this.userName = userName;
+      this.comentario = comentario;
+  }
+}
