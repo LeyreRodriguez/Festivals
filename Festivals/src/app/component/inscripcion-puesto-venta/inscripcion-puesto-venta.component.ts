7893c4c5f74db6b9aa93061a33efffc60bb15fd7
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-05-20 20:47:48 +0100

    Venta en el festival

--- Festivals/src/app/component/inscripcion-puesto-venta/inscripcion-puesto-venta.component.ts
+++ Festivals/src/app/component/inscripcion-puesto-venta/inscripcion-puesto-venta.component.ts
@@ -2,8 +2,9 @@
 import { festival } from 'src/app/models/festival/festival';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-//import { ToastrService } from 'ngx-toastr';
 import { InscripcionPuestoVentaService } from 'src/app/services/inscripcion-puesto-venta.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { FestivalService } from 'src/app/services/festival-service.service';
 
 @Component({
   selector: 'app-inscripcion-puesto-venta',
@@ -15,12 +16,14 @@ export class InscripcionPuestoVentaComponent implements OnInit {
   loading = false;
   overlay = false;
   id: string | undefined;
-  listFestivales: festival[] = [];
+  idFestival_: string | undefined;
+  objFestival: Array<festival> = new Array();
 
   constructor(
         private fb: FormBuilder,
         private inscripcionPuestoVentaService: InscripcionPuestoVentaService,
-        //private toastr: ToastrService,
+        private festivalService: FestivalService,
+        private rutaActiva: ActivatedRoute,
       ) {
     this.ventaForm = this.fb.group({
       inputContestName: ['', Validators.required],
@@ -35,7 +38,17 @@ export class InscripcionPuestoVentaComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.obtenerFestivales();
+    this.obtenerFestival();
+    this.obtenerId();
+  }
+
+  obtenerId(){
+    this.rutaActiva.params.subscribe(
+      (params: Params) => {
+        this.idFestival_ = params['id'];
+      }
+    );
+    console.log('Id = ' + this.idFestival_);
   }
 
   agregarVenta() {
@@ -51,16 +64,18 @@ export class InscripcionPuestoVentaComponent implements OnInit {
       inputDescription: this.ventaForm.value.inputDescription,
     }
 
+    console.log('Holaaaaaaa');
+    puestoVenta.idFestival = this.idFestival_;
+    console.log('Adiossssss');
+
     this.loading = true;
     this.inscripcionPuestoVentaService.guardarVenta(puestoVenta).then(() => {
       this.loading = false;
       console.log('tarjeta registrado');
-     // this.toastr.success('El usuario fue registrado con exito en el concurso!', 'Concurso registrado')
       this.ventaForm.reset();
     }, error => {
       this.loading = false;
       this.overlay = true;
-     // this.toastr.error('Opps.. ocurrio un error', 'Error');
       console.log(error);
     })
   }
@@ -71,16 +86,16 @@ export class InscripcionPuestoVentaComponent implements OnInit {
     }
   }
 
-  obtenerFestivales(){
-    this.inscripcionPuestoVentaService.obtenerFestivales().subscribe(doc => {
-      this.listFestivales = [];
+  obtenerFestival(){
+    this.festivalService.obtenerFestival().subscribe(doc => {
       doc.forEach((element: any) => {
-        this.listFestivales.push({
-          id: element.payload.doc.id,
-          ...element.payload.doc.data()
-        })
+        if(this.idFestival_ == element.payload.doc.data().id){
+          this.objFestival.push({
+            id: element.payload.doc.id,
+            ...element.payload.doc.data()
+          })
+        }
       });
-      console.log(this.listFestivales);
     })
   }
 }
