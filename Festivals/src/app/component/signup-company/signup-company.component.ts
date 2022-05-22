73d2a566da1a2d53d10b5b05f98306e97c17a6c8
selenegc01 <99739761+selenegc01@users.noreply.github.com>
2022-05-22 01:05:55 +0100

    Registro empresa

--- Festivals/src/app/component/signup-company/signup-company.component.ts
+++ Festivals/src/app/component/signup-company/signup-company.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/interfaces/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-company',
@@ -7,18 +10,38 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class SignupCompanyComponent implements OnInit {
 
-  signupBussinesForm: FormGroup;
-  constructor(private fb: FormBuilder) {
-    this.signupBussinesForm = this.fb.group({
+  registerForm: FormGroup;
 
-      inputBussinesName: ['', Validators.required],
-      inputEmail: ['', [Validators.required, Validators.email]],
-      inputPassword: ['', [Validators.required, Validators.pattern(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,15}$/)]]
-
+  constructor(
+    private fb: FormBuilder, 
+    private authSvc: AuthService, 
+    private router: Router,) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,15}$/)]],
     })
   }
-
+  
   ngOnInit(): void {
   }
 
+  onRegister() {
+    const password = this.registerForm.get('password')?.value
+
+    const user: User = {
+      firstName: this.registerForm.value.firstName,
+      lastName: this.registerForm.value.firstName,
+      email: this.registerForm.value.email,
+      photoUrl: "",
+      company: true,
+      admin: false,
+      festivalesFavoritos: [],
+      userName: this.registerForm.value.firstName,
+    }
+
+    console.log(user);
+    this.authSvc.register(user, password);
+    this.router.navigate(['/', 'mainPage']);
+  }
 }
